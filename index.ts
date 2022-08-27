7212f6121900f3fff35a22b586546875e9dd786a
larguesa <rlarguesa@gmail.com>
2022-08-26 22:03:25 -0300

    Class person, index.ts and index.html

--- index.ts
+++ index.ts
@@ -1,3 +1,19 @@
-// Write TypeScript code!
-//const appDiv: HTMLElement = document.getElementById('app');
-//appDiv.innerHTML = `<h1>My TypeScript Starter</h1>`;
+import { Person } from './person';
+
+let me: Person = new Person();
+me.firstname = 'Ricardo';
+me.middlename = 'Pupo';
+me.lastname = 'Larguesa';
+me.age = 43;
+me.emails.push('mail-one@domain.com');
+me.emails.push('mail-two@domain.com');
+
+let e: HTMLElement;
+e = document.getElementById('person-name');
+e.innerText = me.getFullName();
+e = document.getElementById('person-emails');
+e.innerText = me.emails.toString();
+e = document.getElementById('person-age');
+e.innerText = me.age.toString();
+e = document.getElementById('person-birthday');
+e.innerText = me.getBirthdayYear().toString();
--- person.ts
+++ person.ts
@@ -1,13 +1,13 @@
-export class Person{
+export class Person {
   firstname: string;
   middlename: string;
   lastname: string;
   age: number;
-  emails: Array<string>;
-  getFullName(){
+  emails: Array<string> = new Array();
+  getFullName() {
     return this.firstname + ' ' + this.middlename + ' ' + this.lastname;
-  } 
-  getBirthdayYear(){
+  }
+  getBirthdayYear() {
     return new Date().getFullYear() - this.age;
   }
 }
